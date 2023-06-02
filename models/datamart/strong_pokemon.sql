8b65c371b140be78d65ae86a3bf120e6a53d49d4
gabrieldudu <gabriel.rdudu@gmail.com>
2023-06-02 15:57:35 +0300

    update file

--- models/datamart/strong_pokemon.sql
+++ models/datamart/strong_pokemon.sql
@@ -2,10 +2,10 @@ select
     a.name,
     b.base_stat status_value,
     c.name status_name
-from {{ ref('pokemon') }} a
-left join {{ ref('pokemon_stats') }} b
+from {{ ref('poke_pokemon') }} a
+left join {{ ref('poke_pokemon_stats') }} b
   on a._airbyte_pokemon_hashid = b._airbyte_pokemon_hashid
-left join {{ ref('pokemon_stats_stat') }} c
+left join {{ ref('poke_pokemon_stats_stat') }} c
    on b._airbyte_stats_hashid = c._airbyte_stats_hashid
 where c.name = 'attack'
 order by status_value desc
