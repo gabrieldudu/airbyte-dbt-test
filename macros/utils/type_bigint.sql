d4baff43ad4531327e6eb40263622a8d2412a7c5
gabrieldudu <gabriel.rdudu@gmail.com>
2023-06-06 13:09:44 +0300

    update fisier

--- macros/utils/type_bigint.sql
+++ macros/utils/type_bigint.sql
@@ -1,3 +1,9 @@
 {%- macro type_bigint() -%}
   {{ adapter.dispatch('type_bigint', packages = ['dbt_utils'])() }}
 {%- endmacro -%}
+
+
+
+{%- macro postgres__type_bigint() -%}
+  {{ adapter.dispatch('postgres__type_bigint', packages = ['dbt_utils'])() }}
+{%- endmacro -%}
